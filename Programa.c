d4b83c5ba7c2db74d56ba0fa7741375bc3967da1
mayaamdn <a01276295@tec.mx>
2025-11-05 19:29:46 -0600

    Update Programa.c

--- Programa.c
+++ Programa.c
@@ -1,62 +1,15 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2865
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;\red239\green239\blue239;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c94902\c94902\c94902;\cssrgb\c0\c0\c0;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs29\fsmilli14726 \cf0 \cb2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec3 #include <stdio.h>\strokec0 \
-\pard\pardeftab720\partightenfactor0
-
-\fs24 \cf0 \strokec3 \
-\pard\pardeftab720\partightenfactor0
-
-\fs29\fsmilli14726 \cf0 #include <stdlib.h>\strokec0 \
-\pard\pardeftab720\partightenfactor0
-
-\fs24 \cf0 \strokec3 \
-\pard\pardeftab720\partightenfactor0
-
-\fs29\fsmilli14726 \cf0 #include <math.h>\strokec0 \
-\pard\pardeftab720\partightenfactor0
-
-\fs24 \cf0 \strokec3 \
-\pard\pardeftab720\partightenfactor0
-
-\fs29\fsmilli14726 \cf0 int main()\{\strokec0 \
-\pard\pardeftab720\partightenfactor0
-
-\fs24 \cf0 \strokec3 \
-\pard\pardeftab720\partightenfactor0
-
-\fs29\fsmilli14726 \cf0 int length;\strokec0 \
-\pard\pardeftab720\partightenfactor0
-
-\fs24 \cf0 \strokec3 \
-\pard\pardeftab720\partightenfactor0
-
-\fs29\fsmilli14726 \cf0 scanf("%i", &length);\strokec0 \
-\pard\pardeftab720\partightenfactor0
-
-\fs24 \cf0 \strokec3 \
-\pard\pardeftab720\partightenfactor0
-
-\fs29\fsmilli14726 \cf0 int office = pow(2,length\strokec0 \
-\strokec3 );\strokec0 \
-\pard\pardeftab720\partightenfactor0
-
-\fs24 \cf0 \strokec3 \
-\pard\pardeftab720\partightenfactor0
-
-\fs29\fsmilli14726 \cf0 printf("%\strokec0 \
-\strokec3 i\strokec0 \
-\strokec3 ",office);\strokec0 \
-\pard\pardeftab720\partightenfactor0
-
-\fs24 \cf0 \strokec3 \
-\pard\pardeftab720\partightenfactor0
-
-\fs29\fsmilli14726 \cf0 return 0;\strokec0 \
-\}}
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+12
+int main(){
+int num1 = 12;
+int num2 = 3;
+int op1 = suma(num1,num2);
+printf("%i",op1);
+int op2 = resta(num1,num2);
+printf("%i",op2);
+int op3 = multi(num1,num2);
+printf("%i",op3);
+return 0;
+}
